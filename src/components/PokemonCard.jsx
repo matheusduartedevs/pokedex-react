47c02d3dd6292ed500eac9196016555c6a9fd445
unknown <matheus.duartesilva42@gmail.com>
2023-06-27 08:48:12 -0300

    api

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '/src/styles/PokemonCard.css'
 
 const PokemonCard = () => {
-    return (
-        <div>
-            <div className="card-container">
-                <div className="pokemon-card">
-                    <img src="/" alt="Pokemon" />
-                    <h3>Pokemon Nome</h3>
+    const [pokemonCard, setPokemonCard] = useState([])
+
+    const searchPokemon = () => {
+        try {
+            fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    setPokemonCard(data)
+                })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        searchPokemon()
+    }, [])
+
+                return (
+                <div>
+                    <div className="card-container">
+                        <div className="pokemon-card">
+                            {
+                                
+                            }
+                            <img src={data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']} alt="Pokemon" />
+                            <h3>{data.name}</h3>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
+            )
 }
 
 export default PokemonCard
